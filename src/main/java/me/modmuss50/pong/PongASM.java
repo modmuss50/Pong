a27ffa843166bcae8af1a173ab5a2cdedd2be311
modmuss50 <modmuss50@gmail.com>
2016-05-19 19:47:35 +0100

    A mod appears

---
+++ src/main/java/me/modmuss50/pong/PongASM.java
@@ -0,0 +1,42 @@
+package me.modmuss50.pong;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+/**
+ * Created by modmuss50 on 19/05/2016.
+ */
+// -Dfml.coreMods.load=me.modmuss50.pong.PongASM
+@IFMLLoadingPlugin.Name(value = "Pong ASM")
+@IFMLLoadingPlugin.MCVersion(value = "1.9.4")
+public class PongASM implements IFMLLoadingPlugin {
+
+
+    public static boolean deobfuscatedEnvironment = false;
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[] { PongClassTransformer.class.getName() };
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        deobfuscatedEnvironment = !((Boolean)data.get("runtimeDeobfuscationEnabled")).booleanValue();
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
